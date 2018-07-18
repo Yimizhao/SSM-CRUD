ea6e27a5bc6b7a897cdf38224e62d9d62d1d29ea
Yimizhao <zhao12051610@163.com>
2018-07-18 15:43:09 +0800

    追加数据库文件

---
+++ ssm_crud.sql
@@ -0,0 +1,67 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.7.21-log : Database - ssm_crud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm_crud` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssm_crud`;
+
+/*Table structure for table `tbl_dept` */
+
+DROP TABLE IF EXISTS `tbl_dept`;
+
+CREATE TABLE `tbl_dept` (
+  `dept_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_dept` */
+
+/*Table structure for table `tbl_emp` */
+
+DROP TABLE IF EXISTS `tbl_emp`;
+
+CREATE TABLE `tbl_emp` (
+  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `emp_name` varchar(255) NOT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `d_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`),
+  KEY `fk_emp_dept` (`d_id`),
+  CONSTRAINT `fk_emp_dept` FOREIGN KEY (`d_id`) REFERENCES `tbl_dept` (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_emp` */
+
+/*Table structure for table `tbl_employee` */
+
+DROP TABLE IF EXISTS `tbl_employee`;
+
+CREATE TABLE `tbl_employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_name` varchar(30) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_employee` */
+
+insert  into `tbl_employee`(`id`,`last_name`,`gender`,`email`) values (2,'Lee','0','123@2564'),(4,'0000000004','4','0000000004'),(8,'Lee','0','123@2564'),(9,'Lee','0','123@2564'),(10,'Lee','0','123@2564'),(11,'er','1','dff');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
